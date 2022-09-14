5053c85ac07694ddd072dd13212563d5ec173fe8
Danielleal611 <danielantonioleal611@gmail.com>
2022-09-14 10:23:58 -0500

    actualizacion

--- form.js
+++ form.js
@@ -2,7 +2,7 @@ function validar_formulario(){
     var nombre = document.formDatosPersonales.user;
     var nombre_len = nombre.value.length;
     if (nombre_len == 0 || nombre_len < 6){
-        alert("el usuario debe tener minimo 6 caracteres.");
+        alert("el nombre de usuario debe tener minimo 6 caracteres.");
     }
 
 }
