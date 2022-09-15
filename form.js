bcfdff06f0e3291f1c9f7eaa79e72507effdcc76
Danielleal611 <danielantonioleal611@gmail.com>
2022-09-15 09:17:55 -0500

    cambios

--- form.js
+++ form.js
@@ -4,5 +4,10 @@ function validar_formulario(){
     if (nombre_len == 0 || nombre_len < 6){
         alert("el nombre de usuario debe tener minimo 6 caracteres.");
     }
+    var contraseña = document.formDatosPersonales.password;
+    var contraseña_len = contraseña.value.length;
+    if (contraseña_len == 0 || contraseña_len < 6){
+        alert("la contraseña debe tener minimo 6 caracteres.");
+    }
 
-}
+}  
